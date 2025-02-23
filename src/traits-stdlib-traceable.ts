8b604cc6bfacb7ac73944a049d4128a1996419ab
Ralf S. Engelschall <rse@engelschall.com>
2025-02-23 23:59:59 +0100

    replace Moment.js with a local date formatting function

--- src/traits-stdlib-traceable.ts
+++ src/traits-stdlib-traceable.ts
@@ -5,11 +5,23 @@
 */
 
 import { trait }    from "@rse/traits"
-import moment       from "moment"
 
 /*  the default types  */
 type TraceLogLevels    = string
 type TraceLogLevelsDef = "FATAL" | "ERROR" | "WARNING" | "INFO" | "DEBUG"
+
+/*  utility function for formatting a date  */
+const formatDate = (date: Date) => {
+    const pad = (value: number, len: number) => value.toString().padStart(len, "0")
+    const year    = date.getFullYear()
+    const month   = pad(date.getMonth() + 1, 2)
+    const day     = pad(date.getDate(), 2)
+    const hours   = pad(date.getHours(), 2)
+    const minutes = pad(date.getMinutes(), 2)
+    const seconds = pad(date.getSeconds(), 2)
+    const ms      = pad(date.getMilliseconds(), 3)
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${ms}`
+}
 
 /*  the API trait "Traceable<T>"  */
 export const Traceable = <T extends TraceLogLevels = TraceLogLevelsDef>() =>
@@ -28,7 +40,7 @@ export const Traceable = <T extends TraceLogLevels = TraceLogLevelsDef>() =>
         const idxLevel1 = this.$logLevels.indexOf(logLevel)
         const idxLevel2 = this.$logLevels.indexOf(this.$logLevel)
         if (idxLevel1 <= idxLevel2) {
-            const timestamp = moment().format("YYYY-MM-DD hh:mm:ss.SSS")
+            const timestamp = formatDate(new Date())
             let line = `${timestamp}: [${logLevel}] ${msg}`
             if (data)
                 line += " (" + Object.keys(data).map((key) =>
